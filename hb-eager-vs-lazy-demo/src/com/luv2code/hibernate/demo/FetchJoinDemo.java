25be112dfb0b5525dd4938d7d77154811436693d
andreijivan <andrei_jivian@yahoo.com>
2021-01-20 10:54:44 +0200

    Final Commit

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,59 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+			//How to get tempInstructor courses and bypass lazy loading.
+			//option 2: Hibernate query with HQL
+			int theId=1;
+			Query<Instructor> query = session.createQuery("select i from Instructor i "
+														+ "JOIN FETCH i.courses "
+														+ "where i.id=:theInstructorId", 
+														Instructor.class);
+			//set parameter on query
+			query.setParameter("theInstructorId", theId);
+			
+			//execute query and get instructor
+			Instructor tempInstructor = query.getSingleResult();
+			
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+			
+			session.getTransaction().commit();
+			session.close();
+			
+			System.out.println("\nluv2code: The session is now closed!\n");
+			
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+		
+			System.out.println("luv2code: Done!");
+			
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
